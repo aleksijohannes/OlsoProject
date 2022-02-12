fea348349d0884a5b017867494c7ee387270908c
jennijavanainen <jenni.kupukupu@gmail.com>
2022-02-12 12:57:27 +0200

    Rajapintoihin lisätty metodit käyttäjän syötteiden käsittelyyn ja kuljettamiseen viewistä modeliin. Kontrolleriin lisätty toiminnallisuus palvelupisteiden yksilöllisten jakaumien luontiin. Sopivia jakauma-arvoja täytyy vielä testailla ja muokata.

--- simulaattorinrunko5/src/controller/IKontrolleriMtoV.java
+++ simulaattorinrunko5/src/controller/IKontrolleriMtoV.java
@@ -1,10 +1,24 @@
 package controller;
 
+import eduni.distributions.Generator;
+
 public interface IKontrolleriMtoV {
 	
 		// Rajapinta, joka tarjotaan moottorille:
 		
 		public void naytaLoppuaika(double aika);
 		public void visualisoiAsiakas();
+		
+		public int getOviMaara();
+		public int getIlmoMaara();
+		public int getRokMaara();
+		public int getSeurMaara();
+		
+		public Generator getOviJakauma();
+		public Generator getIlmoJakauma();
+		public Generator getRokJakauma();
+		public Generator getSeurJakauma();
+		
+		public Generator getSaapumisjakauma();
 
 }
