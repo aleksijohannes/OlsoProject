fb9ac5d62f388ec0803767b5e29ac1fc7e2c673e
jennijavanainen <jenni.kupukupu@gmail.com>
2022-02-14 15:59:48 +0200

    Lisää rajapintametodeja

--- simulaattorinrunko5/src/controller/IKontrolleriMtoV.java
+++ simulaattorinrunko5/src/controller/IKontrolleriMtoV.java
@@ -1,12 +1,16 @@
 package controller;
 
 import eduni.distributions.Generator;
+import simu.model.PalvelupisteenTyyppi;
 
 public interface IKontrolleriMtoV {
 	
 		// Rajapinta, joka tarjotaan moottorille:
 		
 		public void naytaLoppuaika(double aika);
+		public void naytaAvgLapimeno(double aika);
+		public void naytaKayttoaste(int id, PalvelupisteenTyyppi tyyppi, double kayttoaste);
+		public void naytaSuoritusteho(int id, PalvelupisteenTyyppi tyyppi, double suoritusteho);
 		public void visualisoiAsiakas();
 		
 		public int getOviMaara();
