1bc4bf675ce09474d93d890e86b0ca0d363550d6
jennityn <jenni.tynkkynen@metropolia.fi>
2022-03-06 12:49:57 +0200

    OmaMoottoriin lisätty metodit pal.pistekohtaisten jonotusaikojen min, max ja avg arvoille

--- simulaattorinrunko5/src/simu/model/Asiakas.java
+++ simulaattorinrunko5/src/simu/model/Asiakas.java
@@ -1,5 +1,8 @@
 package simu.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import simu.framework.Kello;
 import simu.framework.Trace;
 
@@ -9,10 +12,10 @@ import simu.framework.Trace;
 public class Asiakas {
 	private double saapumisaika;
 	private double poistumisaika;
+	private double lapimenoaika;
 	private int id;
 	private static int i = 1;
 	private static long sum = 0;
-	//tekstimuutos
 	
 	public Asiakas(){
 	    id = i++;
@@ -39,6 +42,11 @@ public class Asiakas {
 	
 	public int getId() {
 		return id;
+	}
+	
+	public double asiakkaanLapimeno() {
+		lapimenoaika = poistumisaika-saapumisaika;
+		return lapimenoaika;
 	}
 	
 	public void raportti(){
