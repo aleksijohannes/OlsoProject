f365aacbd0498a3fd060096576f56f9f8a023311
jennijavanainen <jenni.kupukupu@gmail.com>
2022-03-11 16:17:00 +0200

    Rajapintojen javadocit tehty

--- simulaattorinrunko5/src/controller/IKontrolleriVtoM.java
+++ simulaattorinrunko5/src/controller/IKontrolleriVtoM.java
@@ -3,21 +3,39 @@ package controller;
 import java.util.ArrayList;
 
 /**
- * Rajapinta sisältää tiedon metodeista, jotka toteutetaan kontrollerissa, ja joita käyttöliittymä kutsuu.
+ * Rajapinta sisältää tiedon metodeista, jotka toteutetaan kontrollerissa, ja
+ * joita käyttöliittymä kutsuu.
  * 
  * @author Jenni Javanainen
  */
 
 public interface IKontrolleriVtoM {
-		
-		// Simulaation ajoon liittyvät metodit
-		public void kaynnistaSimulointi();
-		public void nopeuta();
-		public void hidasta();
-		
-		// Tietokannan kanssa kommunikointiin tarvittavat metodit
-		public void tallennaSimulaatio();
-		public ArrayList<Simulaatio> haeSimulaatiot();
-		
-		
+
+	/**
+	 * Käynnistää simuloinnin
+	 */
+	public void kaynnistaSimulointi();
+
+	/**
+	 * Nopeuttaa simulointia
+	 */
+	public void nopeuta();
+
+	/**
+	 * Hidastaa simulointia
+	 */
+	public void hidasta();
+
+	/**
+	 * Tallentaa uuden simulaation tietokantaan
+	 */
+	public void tallennaSimulaatio();
+
+	/**
+	 * Hakee ja palauttaa tallennetut simulaatiot tietokannasta
+	 * 
+	 * @return simulaatiot listamuodossa
+	 */
+	public ArrayList<Simulaatio> haeSimulaatiot();
+
 }
